53cfb7fb73754669956b180fdfd61b2d027e486f
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:33:05 -0500

    test: initial commit

---
+++ src/test/java/edu/brown/cs/student/main/server/TestLoadCsvHandler.java
@@ -0,0 +1,12 @@
+package edu.brown.cs.student.main.server;
+
+/** A class for unit testing of the functionality of the LoadCsvHandler class. */
+public class TestLoadCsvHandler {
+
+  // http://localhost:3232/loadcsv (should error)
+
+  // http://localhost:3232/loadcsv?filepath=empty.csv&headersIncluded=true
+
+  // http://localhost:3232/loadcsv?filepath=RI_City_Town.csv&headersIncluded=true
+
+}
