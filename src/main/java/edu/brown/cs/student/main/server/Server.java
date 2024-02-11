76135413988399f84372327bb92fa844b7bd60d0
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:11:23 -0500

    fix: moved server port initialization to Server constructor, added handling for loadcsv endpoint

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -1,29 +1,36 @@
 package edu.brown.cs.student.main.server;
 
 import edu.brown.cs.student.main.server.acs.CensusHandler;
+import edu.brown.cs.student.main.server.csv.LoadCsvHandler;
 import edu.brown.cs.student.main.server.csv.searching.Searcher;
 import spark.Spark;
 
 import static spark.Spark.after;
 
 public class Server {
-    public static void main(String[] args) {
-        int port = 3232;
-        Searcher csvSearcher;
+    static final int port = 3232;
 
-        Spark.port(port);
+    // TODO: Eventually should take an AcsDataSource (or whatever we end up naming the interface
+    //  describing the census datasource to use) for mocking purposes in testing
+    public Server() {
+        Spark.port(this.port);
         after(
-                (request, response) -> {
-                    response.header("Access-Control-Allow-Origin", "*");
-                    response.header("Access-Control-Allow-Methods", "*");
-                });
+            (request, response) -> {
+                response.header("Access-Control-Allow-Origin", "*");
+                response.header("Access-Control-Allow-Methods", "*");
+            });
+
+        Spark.get("/broadband", new CensusHandler());
 
         // /loadcsv?filepath=String&headersIncluded=boolean
-        // somehow put in instructions that filepath should be path from data folder?
+        // somehow add instructions that filepath should be path from data folder?
+        Spark.get("/loadcsv", new LoadCsvHandler());
 
-        Spark.get("/broadband",new CensusHandler());
-        Spark.init();
         Spark.awaitInitialization();
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
         System.out.println("Server started at http://localhost:" + port);
     }
 }
