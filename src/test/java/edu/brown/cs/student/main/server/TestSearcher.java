905cbc74baba510c79ec47e70e41340f91f7fc4f
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:10:43 -0500

    refactor: moved to new server package under test

--- src/test/java/edu/brown/cs/student/main/server/TestSearcher.java
+++ src/test/java/edu/brown/cs/student/main/server/TestSearcher.java
@@ -1,4 +1,4 @@
-package edu.brown.cs.student;
+package edu.brown.cs.student.main.server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
