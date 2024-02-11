62a66ba6edbf739d246cde57e7bb802f00521394
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:10:21 -0500

    fix: made abstract class, added static Searcher for csv endpoint handlers to use

--- src/main/java/edu/brown/cs/student/main/server/csv/CsvHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/csv/CsvHandler.java
@@ -1,11 +1,14 @@
 package edu.brown.cs.student.main.server.csv;
 
+import edu.brown.cs.student.main.server.csv.searching.Searcher;
 import spark.Request;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CsvHandler {
+public abstract class CsvHandler {
+    protected static Searcher csvSearcher;
+
     /**
      * Converts a String to a boolean and throws exception if given something other than case-insensitive true or false.
      *
@@ -24,13 +27,13 @@ public class CsvHandler {
     }
 
     /**
-     * Sterilizes exception due to bad request to return as json.
+     * Creates a Map from labels to details for describing an error due to a bad request.
      *
-     * @param request, the Request that caused the exception
+     * @param request the Request that caused the exception
      * @param exn the Exception to report back to API caller
-     * @return the sterilized Object version of exn
+     * @return a Map describing the error
      */
-    protected Object sterilizeBadRequestError(Request request, Exception exn) {
+    protected Map<String, Object> mapBadRequestError(Request request, Exception exn) {
         Map<String, Object> response = new HashMap<>();
         response.put("request", request.body());
         response.put("result", "error_bad_request");
@@ -39,13 +42,13 @@ public class CsvHandler {
     }
 
     /**
-     * Sterilizes exception due to invalid data source to return as json.
+     * Creates a Map from labels to details for describing an error due to an invalid data source.
      *
-     * @param request, the Request that caused the exception
+     * @param request the Request that caused the exception
      * @param exn the Exception to report back to API caller
-     * @return the sterilized Object version of exn
+     * @return a Map describing the error
      */
-    protected Object sterilizeErrorDatasource(Request request, Exception exn) {
+    protected Map<String, Object> mapErrorDatasource(Request request, Exception exn) {
         Map<String, Object> response = new HashMap<>();
         response.put("request", request.body());
         response.put("result", "error_datasource");
