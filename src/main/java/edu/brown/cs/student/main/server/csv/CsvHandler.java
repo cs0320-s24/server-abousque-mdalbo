e6f247f3df4da06e83f8da997843878b9b98fe86
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:32:49 -0500

    fix: changed Request.body() calls to the more appropriate Request.queryString()

--- src/main/java/edu/brown/cs/student/main/server/csv/CsvHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/csv/CsvHandler.java
@@ -1,58 +1,58 @@
 package edu.brown.cs.student.main.server.csv;
 
 import edu.brown.cs.student.main.server.csv.searching.Searcher;
-import spark.Request;
-
 import java.util.HashMap;
 import java.util.Map;
+import spark.Request;
 
 public abstract class CsvHandler {
-    protected static Searcher csvSearcher;
+  protected static Searcher csvSearcher;
 
-    /**
-     * Converts a String to a boolean and throws exception if given something other than case-insensitive true or false.
-     *
-     * @param boolString the String to convert to a boolean
-     * @return the boolean version of boolString
-     * @throws IllegalArgumentException if boolString is neither "true" nor "false" (case-insensitive)
-     */
-    protected boolean parseBoolean(String boolString) throws IllegalArgumentException {
-        if (boolString.equalsIgnoreCase("true")) {
-            return true;
-        } else if (boolString.equalsIgnoreCase("false")) {
-            return false;
-        } else {
-            throw new IllegalArgumentException("Cannot convert '" + boolString + "' to boolean.");
-        }
+  /**
+   * Converts a String to a boolean and throws exception if given something other than
+   * case-insensitive true or false.
+   *
+   * @param boolString the String to convert to a boolean
+   * @return the boolean version of boolString
+   * @throws IllegalArgumentException if boolString is neither "true" nor "false" (case-insensitive)
+   */
+  protected boolean parseBoolean(String boolString) throws IllegalArgumentException {
+    if (boolString.equalsIgnoreCase("true")) {
+      return true;
+    } else if (boolString.equalsIgnoreCase("false")) {
+      return false;
+    } else {
+      throw new IllegalArgumentException("Cannot convert '" + boolString + "' to boolean.");
     }
+  }
 
-    /**
-     * Creates a Map from labels to details for describing an error due to a bad request.
-     *
-     * @param request the Request that caused the exception
-     * @param exn the Exception to report back to API caller
-     * @return a Map describing the error
-     */
-    protected Map<String, Object> mapBadRequestError(Request request, Exception exn) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("request", request.body());
-        response.put("result", "error_bad_request");
-        response.put("message", exn.getMessage());
-        return response;
-    }
+  /**
+   * Creates a Map from labels to details for describing an error due to a bad request.
+   *
+   * @param request the Request that caused the exception
+   * @param exn the Exception to report back to API caller
+   * @return a Map describing the error
+   */
+  protected Map<String, Object> mapBadRequestError(Request request, Exception exn) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("request", request.queryString());
+    response.put("result", "error_bad_request");
+    response.put("message", exn.getMessage());
+    return response;
+  }
 
-    /**
-     * Creates a Map from labels to details for describing an error due to an invalid data source.
-     *
-     * @param request the Request that caused the exception
-     * @param exn the Exception to report back to API caller
-     * @return a Map describing the error
-     */
-    protected Map<String, Object> mapErrorDatasource(Request request, Exception exn) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("request", request.body());
-        response.put("result", "error_datasource");
-        response.put("message", exn.getMessage());
-        return response;
-    }
+  /**
+   * Creates a Map from labels to details for describing an error due to an invalid data source.
+   *
+   * @param request the Request that caused the exception
+   * @param exn the Exception to report back to API caller
+   * @return a Map describing the error
+   */
+  protected Map<String, Object> mapErrorDatasource(Request request, Exception exn) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("request", request.queryString());
+    response.put("result", "error_datasource");
+    response.put("message", exn.getMessage());
+    return response;
+  }
 }
--- src/main/java/edu/brown/cs/student/main/server/csv/LoadCsvHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/csv/LoadCsvHandler.java
@@ -5,6 +5,7 @@ import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.server.csv.parsing.FactoryFailureException;
 import edu.brown.cs.student.main.server.csv.searching.Searcher;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,55 +13,52 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.io.IOException;
-
 public class LoadCsvHandler extends CsvHandler implements Route {
 
-    /**
-     * Constructor for LoadCsvHandler class
-     */
-    public LoadCsvHandler() {
-        // TODO: clean up (is this needed?)
-        // could call super() constructor but super doesn't have one anyway so maybe this is unnecessary
+  /** Constructor for LoadCsvHandler class */
+  public LoadCsvHandler() {
+    // TODO: clean up (is this needed?)
+    // could call super() constructor but super doesn't have one anyway so maybe this is unnecessary
+  }
+
+  /**
+   * Interprets and executes user request.
+   *
+   * @param request the Request of the user
+   * @param response the Response to the request // TODO ???? unused?
+   * @return a serialized json describing the results of executing request
+   */
+  @Override
+  public Object handle(Request request, Response response) {
+    String filepath;
+    boolean headersIncluded;
+
+    Moshi moshi = new Moshi.Builder().build();
+    Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
+    JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
+
+    try {
+      filepath = request.queryParams("filepath");
+      headersIncluded = super.parseBoolean(request.queryParams("headersIncluded"));
+      super.csvSearcher = new Searcher(filepath, headersIncluded);
+    } catch (IllegalArgumentException iaExn) {
+      if (iaExn
+          .getMessage()
+          .equals("Invalid CSV path. Please try again with a valid path from ./data/")) {
+        // specific IllegalArgumentException stemming from nonexistent csv data source
+        return adapter.toJson(super.mapErrorDatasource(request, iaExn));
+      } else {
+        // rest of IllegalArgumentExceptions thrown are due to a bad request
+        return adapter.toJson(super.mapBadRequestError(request, iaExn));
+      }
+    } catch (IOException | FactoryFailureException exn) {
+      // occurs when csv can be found but can't be parsed -> bad request
+      return adapter.toJson(super.mapErrorDatasource(request, exn));
     }
 
-    /**
-     * Interprets and executes user request.
-     *
-     * @param request the Request of the user
-     * @param response the Response to the request // TODO ???? unused?
-     * @return a serialized json describing the results of executing request
-     */
-    @Override
-    public Object handle(Request request, Response response) {
-        String filepath;
-        boolean headersIncluded;
-
-        Moshi moshi = new Moshi.Builder().build();
-        Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
-        JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
-
-        try {
-            filepath = request.queryParams("filepath");
-            headersIncluded = super.parseBoolean(request.queryParams("headersIncluded"));
-            super.csvSearcher = new Searcher(filepath, headersIncluded);
-        } catch (IllegalArgumentException iaExn) {
-            if (iaExn.getMessage().equals("Invalid CSV path. Please try again with a valid path from ./data/")) {
-                // specific IllegalArgumentException stemming from nonexistent csv data source
-                return adapter.toJson(super.mapErrorDatasource(request, iaExn));
-            } else {
-                // rest of IllegalArgumentExceptions thrown are due to a bad request
-                return adapter.toJson(super.mapBadRequestError(request, iaExn));
-            }
-        } catch (IOException | FactoryFailureException exn) {
-            // occurs when csv can be found but can't be parsed -> bad request
-            return adapter.toJson(super.mapErrorDatasource(request, exn));
-        }
-
-        Map<String, Object> successResponseMap = new HashMap<>();
-        successResponseMap.put("request", request.body());
-        successResponseMap.put("result", "success");
-        return adapter.toJson(successResponseMap);
-    }
-
+    Map<String, Object> successResponseMap = new HashMap<>();
+    successResponseMap.put("request", request.queryString());
+    successResponseMap.put("result", "success");
+    return adapter.toJson(successResponseMap);
+  }
 }
