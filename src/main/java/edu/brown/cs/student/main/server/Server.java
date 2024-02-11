dba8c22d6356c7dd1b92965ae3761cc99386a874
abigaelbousquet <truthbousquet@gmail.com>
2024-02-10 20:32:10 -0500

    reformat: temporarily commented out census endpoint handling for testing of csv

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -1,36 +1,35 @@
 package edu.brown.cs.student.main.server;
 
-import edu.brown.cs.student.main.server.acs.CensusHandler;
-import edu.brown.cs.student.main.server.csv.LoadCsvHandler;
-import edu.brown.cs.student.main.server.csv.searching.Searcher;
-import spark.Spark;
-
 import static spark.Spark.after;
 
+import edu.brown.cs.student.main.server.csv.LoadCsvHandler;
+import spark.Spark;
+
 public class Server {
-    static final int port = 3232;
+  static final int port = 3232;
 
-    // TODO: Eventually should take an AcsDataSource (or whatever we end up naming the interface
-    //  describing the census datasource to use) for mocking purposes in testing
-    public Server() {
-        Spark.port(this.port);
-        after(
-            (request, response) -> {
-                response.header("Access-Control-Allow-Origin", "*");
-                response.header("Access-Control-Allow-Methods", "*");
-            });
+  // TODO: Eventually should take an AcsDataSource (or whatever we end up naming the interface
+  //  describing the census datasource to use) for mocking purposes in testing
+  public Server() {
+    Spark.port(this.port);
+    after(
+        (request, response) -> {
+          response.header("Access-Control-Allow-Origin", "*");
+          response.header("Access-Control-Allow-Methods", "*");
+        });
 
-        Spark.get("/broadband", new CensusHandler());
+    // commenting this out temporarily so I can test the loadcsv handler -Abby
+    //    Spark.get("/broadband", new CensusHandler());
 
-        // /loadcsv?filepath=String&headersIncluded=boolean
-        // somehow add instructions that filepath should be path from data folder?
-        Spark.get("/loadcsv", new LoadCsvHandler());
+    // /loadcsv?filepath=String&headersIncluded=boolean
+    // somehow add instructions that filepath should be path from data folder?
+    Spark.get("/loadcsv", new LoadCsvHandler());
 
-        Spark.awaitInitialization();
-    }
+    Spark.awaitInitialization();
+  }
 
-    public static void main(String[] args) {
-        Server server = new Server();
-        System.out.println("Server started at http://localhost:" + port);
-    }
+  public static void main(String[] args) {
+    Server server = new Server();
+    System.out.println("Server started at http://localhost:" + port);
+  }
 }
