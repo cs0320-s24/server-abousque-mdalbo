81c78803b6e91727aeac761c0ad7c4bf6c9caeec
abigaelbousquet <truthbousquet@gmail.com>
2024-02-11 12:59:27 -0500

    pair-prog w/ @mdalbo, reformat: initial commit of class framework

---
+++ src/main/java/edu/brown/cs/student/main/server/acs/MockAcsAPI.java
@@ -0,0 +1,19 @@
+package edu.brown.cs.student.main.server.acs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockAcsAPI implements AcsDatasource {
+
+    /**
+     * Queries the ACS broadband data for a specific state and county.
+     *
+     * @param state the State of interest
+     * @param county the County of interest in state
+     * @return a response Map describing the results of this query
+     */
+    public Map<String, Object> queryBroadband(String state, String county) {
+        // TODO: implement
+        return new HashMap<String, Object>();
+    }
+}
