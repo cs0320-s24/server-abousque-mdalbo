a39251c86e1728c9f4359744640757575d07adc9
abigaelbousquet <truthbousquet@gmail.com>
2024-02-12 16:32:22 -0500

    fix: static Searcher made final and initialized with new argument-less constructor

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -4,13 +4,14 @@ import static spark.Spark.after;
 
 import edu.brown.cs.student.main.server.acs.CensusHandler;
 import edu.brown.cs.student.main.server.csv.LoadCsvHandler;
+import edu.brown.cs.student.main.server.csv.SearchCsvHandler;
 import edu.brown.cs.student.main.server.csv.ViewCsvHandler;
 import edu.brown.cs.student.main.server.csv.searching.Searcher;
 import spark.Spark;
 
 public class Server {
   static final int port = 3232;
-  private Searcher csvSearcher;
+  private final Searcher csvSearcher = new Searcher();
 
   // TODO: Server should take an AcsDataSource for mocking purposes in testing
   public Server() {
@@ -21,11 +22,8 @@ public class Server {
           response.header("Access-Control-Allow-Methods", "*");
         });
 
-    // ACS API
     // TODO: Should take AcsDatasource as arg to use for calling API
     Spark.get("/broadband", new CensusHandler());
-
-    // CSV Searcher
 
     // E.g., http://localhost:3232/loadcsv?filepath=String&headersIncluded=boolean
     // TODO: somehow add instructions that filepath should be path from data folder?
@@ -34,9 +32,11 @@ public class Server {
     // E.g., http://localhost:3232/viewcsv
     Spark.get("/viewcsv", new ViewCsvHandler(this.csvSearcher));
 
-    // E.g., http://localhost:3232/searchcsv?target=String
-    //       http://localhost:3232/searchcsv?target=String&
-    //    Spark.get("/searchcsv", new SearchCsvHandler(this.csvSearcher);
+    /* E.g., http://localhost:3232/searchcsv?target=String
+            http://localhost:3232/searchcsv?target=String&columnOfInterest=String
+            http://localhost:3232/searchcsv?target=String&columnOfInterest=num
+    */
+    Spark.get("/searchcsv", new SearchCsvHandler(this.csvSearcher));
 
     Spark.awaitInitialization();
   }
