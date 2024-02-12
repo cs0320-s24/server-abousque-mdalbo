aa0a026dc53650034b75ec73fb23471cb01cfe81
abigaelbousquet <truthbousquet@gmail.com>
2024-02-12 16:29:10 -0500

    feat: added alternate constructor taking no args for purposes of passing to csv api handlers

--- src/main/java/edu/brown/cs/student/main/server/csv/searching/Searcher.java
+++ src/main/java/edu/brown/cs/student/main/server/csv/searching/Searcher.java
@@ -12,14 +12,15 @@ import java.util.List;
 
 /** A class to handle searching through the contents of a CSV file. */
 public class Searcher {
-
+  private boolean initialized;
   private HashMap<String, Integer> columnHeaderToIndex = null;
-  private final List<List<String>> fullCsvContents;
-  private final List<List<String>> csvContents;
-  private final int numCols;
+  private List<List<String>> fullCsvContents;
+  private List<List<String>> csvContents;
+  private int numCols;
 
   /**
-   * Constructor for the Searcher class. Parses user specified CSV file for future searching.
+   * Primary constructor for the Searcher class. Parses user specified CSV file for future
+   * searching.
    *
    * @param pathFromData the path from ./data/ to the CSV file of interest
    * @param headersIncluded true if the first row of the CSV at ./data/pathFromData is a header,
@@ -30,6 +31,42 @@ public class Searcher {
    * @throws FactoryFailureException if a CSV row is unable to be converted into a List of Strings
    */
   public Searcher(String pathFromData, boolean headersIncluded)
+      throws IllegalArgumentException, IOException, FactoryFailureException {
+    initialize(pathFromData, headersIncluded);
+  }
+
+  /**
+   * Alternate constructor for the Searcher class.
+   *
+   * <p>initialize method MUST be called before calling any of this Searcher's other methods.
+   */
+  public Searcher() {
+    this.initialized = false;
+  }
+
+  /**
+   * Indicates whether this Searcher has been initialized (given a CSV file to work with) yet.
+   *
+   * @return true if this has been initialized, false otherwise
+   */
+  public boolean isInitialized() {
+    return this.initialized;
+  }
+
+  /**
+   * Initializes (or re-initializes) this Searcher to the contents of a specific CSV file.
+   *
+   * <p>Also sets this Searcher's "initialized" field to true.
+   *
+   * @param pathFromData the path from ./data/ to the CSV file of interest
+   * @param headersIncluded true if the first row of the CSV at ./data/pathFromData is a header,
+   *     false otherwise
+   * @throws IllegalArgumentException if passed an invalid filepath or headersIncluded but csv is
+   *     empty, or if CSV contains rows of inconsistent lengths
+   * @throws IOException if a line is encountered in the CSV than cannot be read
+   * @throws FactoryFailureException if a CSV row is unable to be converted into a List of Strings
+   */
+  public void initialize(String pathFromData, boolean headersIncluded)
       throws IllegalArgumentException, IOException, FactoryFailureException {
     CsvParser<List<String>> csvParser = generateCsvParser(pathFromData);
     this.fullCsvContents = csvParser.parse();
@@ -48,6 +85,7 @@ public class Searcher {
       // fullCsvContents and csvContents should point to the same list
       this.csvContents = this.fullCsvContents;
     }
+    this.initialized = true;
   }
 
   /**
@@ -86,42 +124,13 @@ public class Searcher {
    * @throws IllegalArgumentException if a row is encountered with a different length than the first
    *     row of the CSV
    */
-  public void checkConsistentRows() throws IllegalArgumentException {
+  private void checkConsistentRows() throws IllegalArgumentException {
     for (List<String> row : this.fullCsvContents) {
       if (row.size() != this.numCols) {
         throw new IllegalArgumentException(
             "Encountered a row with length inconsistent to first row of CSV: " + row);
       }
     }
-  }
-
-  /**
-   * Extracts the CSV contents.
-   *
-   * @return the csv contents associated with this Searcher
-   */
-  public List<List<String>> getCsvContents() {
-    return this.csvContents;
-  }
-
-  /**
-   * Extracts the full CSV contents (including header).
-   *
-   * <p>In the case where the csv doesn't have a header, this is the exact same as getCsvContents.
-   *
-   * @return the full csv contents associated with this Searcher
-   */
-  public List<List<String>> getFullCsv() {
-    return this.fullCsvContents;
-  }
-
-  /**
-   * Extracts the mapping of column names to indices.
-   *
-   * @return the HashMap of column names to their indices
-   */
-  public HashMap<String, Integer> getHeaders() {
-    return this.columnHeaderToIndex;
   }
 
   /**
@@ -173,6 +182,58 @@ public class Searcher {
   }
 
   /**
+   * Extracts the CSV contents.
+   *
+   * @return the csv contents associated with this Searcher
+   * @throws IllegalStateException if called prior to initializing this via initialize method
+   */
+  public List<List<String>> getCsvContents() throws IllegalStateException {
+    if (!this.initialized) {
+      throw new IllegalStateException(
+          "Attempted to retrieve CSV contents before Searcher initialized.");
+    }
+    List<List<String>> copy = new ArrayList<>();
+    for (List<String> row : this.csvContents) {
+      copy.add(new ArrayList<>(row));
+    }
+    return copy;
+  }
+
+  /**
+   * Extracts the full CSV contents (including header).
+   *
+   * <p>In the case where the csv doesn't have a header, this is the exact same as getCsvContents.
+   *
+   * @return the full csv contents associated with this Searcher
+   * @throws IllegalStateException if called prior to initializing this via initialize method
+   */
+  public List<List<String>> getFullCsv() throws IllegalStateException {
+    if (!this.initialized) {
+      throw new IllegalStateException(
+          "Attempted to retrieve CSV contents before Searcher initialized.");
+    }
+    List<List<String>> copy = new ArrayList<>();
+    for (List<String> row : this.fullCsvContents) {
+      copy.add(new ArrayList<>(row));
+    }
+    return copy;
+  }
+
+  /**
+   * Extracts the mapping of column names to indices.
+   *
+   * @return the HashMap of column names to their indices
+   * @throws IllegalStateException if called prior to initializing this via initialize method
+   */
+  public HashMap<String, Integer> getHeaders() throws IllegalStateException {
+    if (!this.initialized) {
+      throw new IllegalStateException(
+          "Attempted to retrieve CSV headers before Searcher initialized.");
+    }
+    return new HashMap<String, Integer>(this.columnHeaderToIndex);
+  }
+
+  /**
    * Searches for a target value in a specific column of CSV contents. A cell (String) in the
    * specified column is considered a match if and only if [lowercase version of
    * cell].equals([lowercase version of target]).
@@ -186,15 +247,17 @@ public class Searcher {
    *     CSV file without a header, or provided empty/null arg
    * @throws IndexOutOfBoundsException if columnOfInterest as Integer lays outside the file's range
    *     or as a column name (String) does not exist
+   * @throws IllegalStateException if called prior to initializing this via initialize method
    */
   public List<List<String>> search(String target, String columnOfInterest)
-      throws IllegalArgumentException, IndexOutOfBoundsException {
-    if (columnOfInterest == null || columnOfInterest.isEmpty()) {
+      throws IllegalArgumentException, IndexOutOfBoundsException, IllegalStateException {
+    if (!this.initialized) {
+      throw new IllegalStateException("Attempted to search before Searcher initialized.");
+    } else if (columnOfInterest == null || columnOfInterest.isEmpty()) {
       throw new IllegalArgumentException(
           "Search passed a null column of interest. "
               + "Please try again with non-null column of interest.");
-    }
-    if (target == null || target.isEmpty()) {
+    } else if (target == null || target.isEmpty()) {
       throw new IllegalArgumentException(
           "Search passed a null target. Please try again with non-null target.");
     }
@@ -204,7 +267,7 @@ public class Searcher {
     String lowercaseTarget = target.toLowerCase();
     for (List<String> row : this.csvContents) {
       if (row.get(colIndex).toLowerCase().equals(lowercaseTarget)) {
-        matchingRows.add(row);
+        matchingRows.add(new ArrayList<>(row));
       }
     }
     return matchingRows;
@@ -218,9 +281,13 @@ public class Searcher {
    * @param target the value to search for
    * @return a List of the rows where any column matched target
    * @throws IllegalArgumentException if given a null or empty target
+   * @throws IllegalStateException if called prior to initializing this via initialize method
    */
-  public List<List<String>> search(String target) throws IllegalArgumentException {
-    if (target == null || target.isEmpty()) {
+  public List<List<String>> search(String target)
+      throws IllegalArgumentException, IllegalStateException {
+    if (!this.initialized) {
+      throw new IllegalStateException("Attempted to search before Searcher initialized.");
+    } else if (target == null || target.isEmpty()) {
       throw new IllegalArgumentException(
           "Search passed a null target. Please try again with non-null target.");
     }
@@ -230,7 +297,7 @@ public class Searcher {
     for (List<String> row : this.csvContents) {
       for (String cell : row) {
         if (cell.toLowerCase().equals(lowercaseTarget)) {
-          matchingRows.add(row);
+          matchingRows.add(new ArrayList<>(row));
           break; // short-circuit row to save some time
         }
       }
