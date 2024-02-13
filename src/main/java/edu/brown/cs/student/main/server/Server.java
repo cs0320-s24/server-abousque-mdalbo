8b95860663c97670c111c62cd856cc8462f54294
abigaelbousquet <truthbousquet@gmail.com>
2024-02-13 12:33:09 -0500

    pair-prog w/ @mdalbo, fix: constructor to take AcsDataSource for testing purposes, cached source configured

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -2,6 +2,8 @@ package edu.brown.cs.student.main.server;
 
 import static spark.Spark.after;
 
+import edu.brown.cs.student.main.server.acs.AcsDatasource;
+import edu.brown.cs.student.main.server.acs.CensusAPI;
 import edu.brown.cs.student.main.server.acs.CensusHandler;
 import edu.brown.cs.student.main.server.csv.LoadCsvHandler;
 import edu.brown.cs.student.main.server.csv.SearchCsvHandler;
@@ -9,44 +11,67 @@ import edu.brown.cs.student.main.server.csv.ViewCsvHandler;
 import edu.brown.cs.student.main.server.csv.searching.Searcher;
 import spark.Spark;
 
-/** A class for running CSV searcher and ACS APIs. */
+/**
+ * A class for running CSV searcher and ACS APIs.
+ *
+ * ACS API is cached in this implementation with cached results held for 4 minutes.
+ */
 public class Server {
   static final int port = 3232;
   private final Searcher csvSearcher = new Searcher();
+  private final AcsDatasource acsDatasource;
 
   /**
    * Constructor for the Server class.
+   *
+   * @param acsDatasource the data source object to use for querying broadband data
    */
-  // TODO: Server should take an AcsDataSource for mocking purposes in testing
-  public Server() {
+  public Server(AcsDatasource acsDatasource) {
     Spark.port(this.port);
+    this.acsDatasource = acsDatasource;
     after(
         (request, response) -> {
           response.header("Access-Control-Allow-Origin", "*");
           response.header("Access-Control-Allow-Methods", "*");
         });
 
-    // TODO: Should take AcsDatasource as arg to use for calling API
-    Spark.get("/broadband", new CensusHandler());
-
-    // E.g., http://localhost:3232/loadcsv?filepath=String&headersIncluded=boolean
-    // TODO: somehow add instructions that filepath should be path from data folder?
+    Spark.get("/broadband", new CensusHandler(this.acsDatasource));
     Spark.get("/loadcsv", new LoadCsvHandler(this.csvSearcher));
-
-    // E.g., http://localhost:3232/viewcsv
     Spark.get("/viewcsv", new ViewCsvHandler(this.csvSearcher));
-
-    /* E.g., http://localhost:3232/searchcsv?target=String
-            http://localhost:3232/searchcsv?target=String&columnOfInterest=String
-            http://localhost:3232/searchcsv?target=String&columnOfInterest=num
-    */
     Spark.get("/searchcsv", new SearchCsvHandler(this.csvSearcher));
 
     Spark.awaitInitialization();
   }
 
+  /**
+   * The main method for running a cached server servicing the
+   * broadband, loadcsv, viewcsv, and searchscv endpoints.
+   *
+   * @param args unused
+   */
   public static void main(String[] args) {
-    Server server = new Server();
+    Server server = new Server(new CensusAPI());
     System.out.println("Server started at http://localhost:" + port);
+    System.out.println(
+        """
+        In a browser window:
+        1. Call the loadcsv endpoint to load in a CSV:
+        http://localhost:port/loadcsv?filepath=String&headersIncluded=boolean
+        *Note: filepath is the filepath from ./data
+
+        2. Call the viewcsv endpoint to view a CSV previously loaded with loadcsv:
+        http://localhost:port/viewcsv
+
+        3. Call the searchcsv endpoint to search a CSV previously loaded with loadcsv:
+        http://localhost:port/searchcsv?target=String
+        http://localhost:port/searchcsv?target=String&columnOfInterest=String
+        http://localhost:port/searchcsv?target=String&columnOfInterest=num
+        *Note: Results will reflect case-insensitive matches to target, and columnOfInterest can be
+               column name if headersIncluded was true in loadcsv call or the column index.
+
+        4. Call the broadband endpoint to query the ACS database for broadband access data:
+        http://localhost:port/broadband?state=String&county=String
+        *Note: States or counties with spaces in their name must use "%20" or "+" rather than " ".
+        """);
   }
 }
