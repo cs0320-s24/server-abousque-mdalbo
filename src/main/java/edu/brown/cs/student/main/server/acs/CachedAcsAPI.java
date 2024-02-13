6036096b53e99ddff207615bf9f11b15d0c065d6
abigaelbousquet <truthbousquet@gmail.com>
2024-02-13 12:33:38 -0500

    pair-prog w/ @mdalbo, feat: implemented caching

--- src/main/java/edu/brown/cs/student/main/server/acs/CachedAcsAPI.java
+++ src/main/java/edu/brown/cs/student/main/server/acs/CachedAcsAPI.java
@@ -1,21 +1,63 @@
 package edu.brown.cs.student.main.server.acs;
 
-import java.util.HashMap;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-/** A proxy class wrapping an AcsDatasource in a cache. */
+/**
+ * A proxy class wrapping an AcsDatasource in a cache. This should be used for querying the Census
+ * (ACS) API with configurable cache.
+ */
 public class CachedAcsAPI implements AcsDatasource {
+
+  private final AcsDatasource wrappedApi;
+  private final LoadingCache<String, Map<String, Object>> cache;
+
+  /**
+   * Proxy class: wrap an instance of Searcher (of any kind) and cache its results.
+   *
+   * @param toWrap the CensusAPI to wrap
+   * @param maxSize the maximum number of queries that should be kept in the cache at a time
+   * @param minutesKept the maximum time in minutes that a query should be kept in the cache
+   */
+  public CachedAcsAPI(AcsDatasource toWrap, int maxSize, int minutesKept) {
+    this.wrappedApi = toWrap;
+    this.cache =
+        CacheBuilder.newBuilder()
+            .maximumSize(maxSize)
+            .expireAfterWrite(minutesKept, TimeUnit.MINUTES)
+            .build(
+                // Strategy pattern: how should the cache behave when
+                // it's asked for something it doesn't have?
+                new CacheLoader<>() {
+                  @Override
+                  public Map<String, Object> load(String codes) {
+                    // Note: codes is "{countyCode},{stateCode}"
+                    String[] codesList = codes.split(",");
+                    String countyCode = codesList[0];
+                    String stateCode = codesList[1];
+                    System.out.println(
+                        "called load for: county #" + countyCode + " under state #" + stateCode);
+                    // If this isn't yet present in the cache, load it:
+                    return wrappedApi.queryBroadband(stateCode, countyCode);
+                  }
+                });
+  }
 
   /**
    * Queries the ACS broadband data for a specific state and county.
    *
-   * @param state the State of interest
-   * @param county the County of interest in state
+   * @param stateCode the ACS code of the State of interest
+   * @param countyCode the ACS code of the County of interest in state
    * @return a response Map describing the results of this query
    */
-  public Map<String, Object> queryBroadband(
-      String stateCode, String countyCode, String state, String county) {
-    // TODO: implement
-    return new HashMap<String, Object>();
+  public Map<String, Object> queryBroadband(String stateCode, String countyCode) {
+    Map<String, Object> result = cache.getUnchecked(countyCode + "," + stateCode);
+
+    // TODO: remove print before submitting
+    System.out.println(cache.stats());
+    return result;
   }
 }
